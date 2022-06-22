05b8f759e9bd39b2a55e3e430f46c3390a55ed4a
RomaChernerga <89411655+RomaChernerga@users.noreply.github.com>
2022-06-22 21:19:47 +0300

    Delete WeatherApp_v23 directory

--- WeatherApp_v23/app/src/main/java/com/example/weatherapp_v2/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp_v2
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
-}
